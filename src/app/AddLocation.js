514f925e5bbba15e154732c2b3d3f45ea5044ca8
Dor Bar <dor4231@gmail.com>
2018-08-19 22:10:36 +0300

    feat: Add two components for the app and add location feture

---
+++ src/app/AddLocation.js
@@ -0,0 +1,5 @@
+import React from 'react'
+const AddLocation = () => (
+  <p>This is the Add Location Page</p>
+)
+export default AddLocation
---
+++ src/app/Home.js
@@ -0,0 +1,5 @@
+import React from 'react'
+const Home = () => (
+  <p>This is the Home Page</p>
+)
+export default Home
