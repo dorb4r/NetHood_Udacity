223a27178db6b285b6c635bd1cd6e1cd4856867e
Dor Bar <dor4231@gmail.com>
2018-08-19 23:36:27 +0300

    feat: Material UI navigation bar

--- src/app/layout/Header.js
+++ src/app/layout/Header.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import PropTypes from 'prop-types';
 
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 class Header extends React.Component {
 
   state = {
@@ -13,54 +21,62 @@ class Header extends React.Component {
     }))
   }
 
+  styles = {
+    root: {
+      flexGrow: 1,
+    },
+    flex: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginLeft: -12,
+      marginRight: 20,
+    },
+  };
+
   render() {
     return (
-      <nav className="navbar"
-          aria-label="main navigation"
-          style={{
-            borderBottom: 'solid 1px #dddddd',
-          }}>
-        <div className="navbar-brand">
-          <a className="navbar-item">
-            <span>NetHood.zone</span>
-          </a>
-          <button className="button navbar-burger" onClick={this.toggleNav}>
-            <span></span>
-            <span></span>
-            <span></span>
-          </button>
-        </div>
-        <div className={ this.state.isActive ? 'navbar-menu is-active' : 'navbar-menu'}>
-          <div className="navbar-start">
-          <NavLink
-            className="navbar-item"
-            to="/"
-            activeClassName="is-active">
-            <span>Home</span>
-          </NavLink>
-          <NavLink
-            className="navbar-item"
-            to="/add_location"
-            activeClassName="is-active">
-            <span>Add Location</span>
-          </NavLink>
-          </div>
-          <div className="navbar-end">
-            <a className="navbar-item" href="https://github.com/dor4231">
-              <span className="icon">
-                <i className="fab fa-lg fa-github"></i>
-              </span>
-            </a>
-            <a className="navbar-item" href="https://www.linkedin.com/in/dor-bar-1491a878/">
-              <span className="icon" style={{ color: '#000', marginLeft: 5 }}>
-                <i className="fab fa-lg fa-linkedin"></i>
-              </span>
-            </a>
-          </div>
-        </div>
-      </nav>
+      <div style={this.styles.root}>
+        <AppBar position="static" color="default">
+          <Toolbar>
+            <IconButton style={this.styles.menuButton}  color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" style={this.styles.flex}>
+            NetHood
+            </Typography>
+            <NavLink
+              style={this.styles.menuButton}
+              to="/"
+              activeClassName="is-active">
+              <span>Home</span>
+            </NavLink>
+            <NavLink
+              style={this.styles.menuButton}
+              to="/add_location"
+              activeClassName="is-active">
+              <span>Add Location</span>
+            </NavLink>
+            <a className="navbar-item" href="https://github.com/dor4231" target="_block">
+                <span className="icon">
+                  <i className="fab fa-lg fa-github"></i>
+                </span>
+              </a>
+              <a className="navbar-item" href="https://www.linkedin.com/in/dor-bar-1491a878/" target="_block">
+                <span className="icon">
+                  <i className="fab fa-lg fa-linkedin"></i>
+                </span>
+              </a>
+          </Toolbar>
+        </AppBar>
+      </div>
     )
   }
 }
 
-export default Header
+
+Header.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(Header.styles)(Header);
