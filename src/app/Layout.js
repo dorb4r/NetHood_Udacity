15a8aaf736cc23e5e5dc55059565e311b5f8a0b7
Dor Bar <dor4231@gmail.com>
2018-08-21 22:18:42 +0300

    refactor: Make Layout stateful component

--- src/app/Layout.js
+++ src/app/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
@@ -8,31 +8,105 @@ import Content from './layout/Content'
 import Footer from './layout/Footer'
 import Router from './layout/Router'
 
-const Layout = ({ children }) => (
-  <Site>
-    <Helmet
-      title="NetHood - Find you're team!"
-      meta={[
-        { name: 'description', content: 'This is a site for finding the best people to team up with.' },
-        { name: 'keywords', content: 'sports, fun, joy, team, friends' },
-      ]}
-      script={[
-        { 'src': 'https://use.fontawesome.com/releases/v5.0.4/js/all.js'},
-      ]}
-      link={[
-        {'rel':'stylesheet', 'href': 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'}
-      ]}
-    />
-    <Header />
-    <Content>
-      <Router />
-    </Content>
-    <Footer />
-  </Site>
-)
+import { withStyles } from '@material-ui/core/styles';
+
+const drawerWidth = 240;
+class Layout extends Component {
+
+  styles = theme => ({
+    root: {
+      flexGrow: 1,
+      height: 430,
+      zIndex: 1,
+      overflow: 'hidden',
+      position: 'relative',
+      display: 'flex',
+    },
+    appBar: {
+      zIndex: theme.zIndex.drawer + 1,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    appBarShift: {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    menuButton: {
+      marginLeft: 12,
+      marginRight: 36,
+      marginBottom: 16,
+    },
+    hide: {
+      display: 'none',
+    },
+    drawerPaper: {
+      position: 'relative',
+      whiteSpace: 'nowrap',
+      width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    drawerPaperClose: {
+      overflowX: 'hidden',
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing.unit * 7,
+      [theme.breakpoints.up('sm')]: {
+        width: theme.spacing.unit * 9,
+      },
+    },
+    toolbar: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'flex-end',
+      padding: '0 8px',
+      ...theme.mixins.toolbar,
+    },
+    content: {
+      flexGrow: 1,
+      backgroundColor: theme.palette.background.default,
+      padding: theme.spacing.unit * 3,
+    },
+  });
+  
+  render() {
+    return (
+      <Site>
+        <Helmet
+          title="NetHood - Find you're team!"
+          meta={[
+            { name: 'description', content: 'This is a site for finding the best people to team up with.' },
+            { name: 'keywords', content: 'sports, fun, joy, team, friends' },
+          ]}
+          script={[
+            { 'src': 'https://use.fontawesome.com/releases/v5.0.4/js/all.js'},
+          ]}
+          link={[
+            {'rel':'stylesheet', 'href': 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'}
+          ]}
+        />
+        <Header />
+        <Content>
+          <Router />
+        </Content>
+        <Footer />
+      </Site>
+    )
+  }
+} 
 
 Layout.propTypes = {
   children: PropTypes.func,
 }
 
-export default Layout
+export default withStyles(Layout.styles, {withTheme: true})(Layout)
