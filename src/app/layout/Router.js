cbfd6f4020897619db0b156055e66b60b1044edb
Dor Bar <dor4231@gmail.com>
2018-08-21 22:18:02 +0300

    chor: Can pass props into the router components

--- src/app/layout/Router.js
+++ src/app/layout/Router.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Home from './../Home'
 import AddLocation from './../AddLocation'
-const Router = () => (
-  <Switch>
-    <Route exact path='/' component={Home}/>
-    <Route path='/add_location' component={AddLocation}/>
-  </Switch>
-)
+
+
+class Router extends Component{
+    render() {
+        return (
+            <Switch>
+              <Route exact path='/'        
+                     render={() =>
+                      <Home />
+                     }/>
+              <Route path='/add_location' 
+                     render={() =>
+                      <AddLocation />
+                     }/>
+            </Switch>
+          )
+    }
+} 
+
 export default Router
