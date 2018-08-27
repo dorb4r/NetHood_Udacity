203ef4c01e549dac431eb2ceea03e561c153fe62
dorb <dor.bar@idomoo.com>
2018-08-27 12:10:50 +0300

    feat: Each marker have it's own InfoWindow
    Save in state if an InfoWindow is open using an object "isOpen".
    This will allow us to open several InfoWindows at once.

--- src/App.js
+++ src/App.js
@@ -1,147 +1,166 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import escapeRegExp from 'escape-string-regexp'
 
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
-import { Button, Input } from '@material-ui/core';
+import {Button, Input} from '@material-ui/core';
+
 import MapComp from './app/MapComp';
+import MyFancyComponent from './app/MapComp';
 
 
 const drawerWidth = 240,
-      API_KEY = "AIzaSyBUTQfI6CTeTw-g7tJwNbFLTy799wzRNeI";
+    API_KEY = "AIzaSyBUTQfI6CTeTw-g7tJwNbFLTy799wzRNeI";
 
 const styles = theme => ({
-  root: {
-    flexGrow: 1,
-    height: "100%",
-    zIndex: 1,
-    overflow: 'hidden',
-    position: 'relative',
-    display: 'flex',
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    display: "flex",
-    justifyContent: "space-between",
-    flexGrow: 1,
-  },
-  drawerPaper: {
-    position: 'relative',
-    width: drawerWidth,
-  },
-  content: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: 0,
-    minWidth: 0, // So the Typography noWrap works
-  },
-  flex: {
-    flexGrow: 1,
-  },
-  toolbar: theme.mixins.toolbar,
+    root: {
+        flexGrow: 1,
+        height: "100%",
+        zIndex: 1,
+        overflow: 'hidden',
+        position: 'relative',
+        display: 'flex',
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+        display: "flex",
+        justifyContent: "space-between",
+        flexGrow: 1,
+    },
+    drawerPaper: {
+        position: 'relative',
+        width: drawerWidth,
+    },
+    content: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.default,
+        padding: 0,
+        minWidth: 0, // So the Typography noWrap works
+    },
+    flex: {
+        flexGrow: 1,
+    },
+    toolbar: theme.mixins.toolbar,
 });
 
 class ClippedDrawer extends Component {
-  state = {
-    locations: [],
-    query: ""
-  }
+    constructor() {
+        super();
+        this.state = {
+            locations: [],
+            query: "",
+            isOpen: {}
+        }
 
-  filterLocations(query) {
-    this.setState({ query });
-  }
-
-  searchLocations(query) {
-    if (query !== "")
-      fetch(`https://api.tomtom.com/search/2/search/${query}.json?key=n9R000qQ4FM75YR9xfDlaiywPO8oSCm4&lat=32.1500&lon=34.8839&radius=3000`)
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data.results)
-          this.setState({locations: data.results})
-        })
-        .catch((err) =>
-          console.log(err)
-        )
-    else this.setState({locations: []})
-  }
-
-  componentDidMount() {
-    fetch("https://api.tomtom.com/search/2/search/sports.json?key=n9R000qQ4FM75YR9xfDlaiywPO8oSCm4&lat=32.1500&lon=34.8839&radius=3000")
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data.results)
-      this.setState({locations: data.results})
-    })
-    .catch((err) =>
-      console.log(err)
-    )
-  }
-
-
-  render() {
-    const { classes } = this.props;
-    const { locations, query } = this.state;
-
-    let showingLocations
-
-    if (query) {
-        const match = new RegExp(escapeRegExp(query), 'i');
-        showingLocations = locations.filter((location) => match.test(location.poi.name));
-    } else {
-      showingLocations = locations;
+        this.onToggleOpen = this.onToggleOpen.bind(this);
+        this.closeWindows = this.closeWindows.bind(this);
     }
 
-    console.log(showingLocations)
+    filterLocations(query) {
+        this.setState({query});
+    }
 
-    return (
-      <div className={classes.root}>
-        <AppBar position="absolute" className={classes.appBar}>
-          <Toolbar className={classes.appBar}>
-            <Typography variant="title" color="inherit" noWrap className={classes.flex}>
-              NetHood
-            </Typography>
-            <form noWrap onSubmit={(e) => {e.preventDefault(); this.searchLocations(e.target.querySelector("#searchLocations").value)}}>
-              <Typography variant="subheading" color="inherit">
-                <label>Load other locations:</label>
-              </Typography>
-              <Input type="text" id="searchLocations" />
-              <Input type="submit" color="inherit" value="load"/>
-            </form>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          variant="permanent"
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-        >
-          <div className={classes.toolbar} />
-          <Input type="text" 
-                  onChange={(value) => this.filterLocations(value.target.value)}/>
-          <Divider />
-          {showingLocations.map((location) => {
-            if(location.type === "POI")
-            return (<Button key={location.id}>{location.poi.name}</Button>);
-          })}
-        </Drawer>
-        <main className={classes.content}>
-          <div className={classes.toolbar} />
-          <MapComp apiKey={API_KEY}
-                   isMarkerShown={true}
-                   markers={this.state.locations}/>
-        </main>
-      </div>
-    );
-  }
+    searchLocations(query) {
+        if (query !== "")
+            fetch(`https://api.tomtom.com/search/2/search/${query}.json?key=n9R000qQ4FM75YR9xfDlaiywPO8oSCm4&lat=32.1500&lon=34.8839&radius=3000`)
+                .then((res) => res.json())
+                .then((data) => {
+                    this.setState({locations: data.results})
+                })
+                .catch((err) => {
+                        console.log(err);
+                        this.setState({locations: []})
+                    }
+                );
+        else this.setState({locations: []})
+    }
+
+    onToggleOpen = (id) => {
+        const isOpen = this.state.isOpen;
+        isOpen[id] = true;
+        this.setState({isOpen});
+    };
+
+    closeWindows = () => {
+        console.log("Close ALL!")
+        // this.setState({ isOpen: {}})
+    };
+
+
+    componentDidMount() {
+        this.searchLocations("sports");
+    }
+
+
+    render() {
+        const {classes} = this.props;
+        const {locations, query} = this.state;
+
+        let showingLocations
+
+        if (query) {
+            const match = new RegExp(escapeRegExp(query), 'i');
+            showingLocations = locations.filter((location) => match.test(location.poi.name));
+        } else {
+            showingLocations = locations;
+        }
+
+        // console.log(showingLocations)
+
+        return (
+            <div className={classes.root}>
+                <AppBar position="absolute" className={classes.appBar}>
+                    <Toolbar className={classes.appBar}>
+                        <Typography variant="title" color="inherit" noWrap className={classes.flex}>
+                            NetHood
+                        </Typography>
+                        <form onSubmit={(e) => {
+                            e.preventDefault();
+                            this.searchLocations(e.target.querySelector("#searchLocations").value)
+                        }}>
+                            <Typography variant="subheading" color="inherit">
+                                <label>Load other locations:</label>
+                            </Typography>
+                            <Input type="text" id="searchLocations"/>
+                            <Input type="submit" color="inherit" value="load"/>
+                        </form>
+                    </Toolbar>
+                </AppBar>
+                <Drawer
+                    variant="permanent"
+                    classes={{
+                        paper: classes.drawerPaper,
+                    }}
+                >
+                    <div className={classes.toolbar}/>
+                    <Input type="text"
+                           onChange={(value) => this.filterLocations(value.target.value)}/>
+                    <Divider/>
+                    {showingLocations.filter((location) => location.type === "POI").map((location) =>
+                        (<Button key={location.id}>{location.poi.name}</Button>)
+                    )}
+                </Drawer>
+                <main className={classes.content}>
+                    <div className={classes.toolbar}/>
+                    <MyFancyComponent apiKey={API_KEY}
+                             isMarkerShown={true}
+                             markers={this.state.locations}
+                             isOpen={this.state.isOpen}
+                             onToggleOpen={this.onToggleOpen}
+                             closeWindows={this.closeWindows}/>
+                </main>
+            </div>
+        );
+    }
 }
 
 ClippedDrawer.propTypes = {
-  classes: PropTypes.object.isRequired,
+    classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles)(ClippedDrawer);
--- src/app/MapComp.js
+++ src/app/MapComp.js
@@ -1,20 +1,11 @@
 /*global google*/
 
 import React from "react"
-import {
-    compose,
-    withProps
-} from "recompose"
-import {
-    withScriptjs,
-    withGoogleMap,
-    GoogleMap,
-    Marker,
-    InfoWindow
-} from "react-google-maps"
+import {compose, withProps} from "recompose"
+import {withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow} from "react-google-maps"
 
 const lat = 32.1500,
-      lng = 34.8839;
+    lng = 34.8839;
 
 const styles = {
     mapContainer: {
@@ -24,85 +15,57 @@ const styles = {
     }
 }
 
-// // class MapComp extends Component {
-
-// //     render() {
-// //         const { apiKey } = this.props;
-// //         return (
-// //             <div style={styles.mapContainer}>
-// //                 <GoogleMap bootstrapURLKeys={{
-// //                             key: apiKey,
-// //                             language: 'en',
-// //                             region: 'il'
-// //                             }}
-// //                             defaultCenter={[lat, lng]}
-// //                             defaultZoom={14}
-// //                         />
-// //             </div>
-// //         )
-
-// //     }
-// // }
-
-
-// // ######################################
-// ######################################
-
 
 const MapComp = compose(
     withProps({
         googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBUTQfI6CTeTw-g7tJwNbFLTy799wzRNeI&v=3.exp&libraries=geometry,drawing,places",
-        loadingElement: <div style = {
-            {
-                height: `100%`
-            }
-        }
-        />,
-        containerElement: < div style = {
-            {
-                height: `calc(100% - 64px)`
-            }
-        }
-        />,
-        mapElement: < div style = {
-            {
-                height: `100%`
-            }
-        }
-        />,
+        loadingElement: <div style={{
+            height: `100%`
+        }}/>,
+        containerElement: < div style={{
+            height: `calc(100% - 64px)`
+        }}/>,
+        mapElement: < div style={{
+            height: `100%`
+        }}/>,
     }),
     withScriptjs,
     withGoogleMap)((props) => {
-        var markerPin = {
+        const markerPin = {
             url: 'https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png',
             scaledSize: new google.maps.Size(42, 43)
         };
-        return (<GoogleMap styles={styles.mapContainer}
-            defaultZoom = {14}
-            defaultCenter = {{lat: lat, lng: lng}}>
+        console.log(props.isOpen);
+        return (
+            <GoogleMap styles={styles.mapContainer}
+                       defaultZoom={14}
+                       defaultCenter={{lat: lat, lng: lng}}>
 
-            {props.markers.map((marker) => {
-                console.log(marker.position.lng);
-                return (<Marker defaultIcon={markerPin}
-                                key={marker.id} 
-                                position={{lat: marker.position.lat, lng: marker.position.lon}}
-                                onClick = {props.onToggleOpen(marker.id)}>
-                            {props.isOpen[marker.id] && 
-                            <InfoWindow onCloseClick={props.closeWindows}>
-                                <p>Dor Bar</p>
-                            </InfoWindow>}
-                        </Marker>)
-                }   
-           )}
-    </GoogleMap>)
+                {props.markers.filter((location) => location.type === "POI")
+                    .map((marker) => {
+                        console.log(marker.position.lng);
+                        if (props.isMarkerShown) {
+                            return (
+                            <Marker defaultIcon={markerPin}
+                                        key={marker.id}
+                                        position={{lat: marker.position.lat, lng: marker.position.lon}}
+                                        onClick={(e) => props.onToggleOpen(marker.id)}>
+                            {props.isOpen[marker.id] && (
+                                <InfoWindow onCloseClick={props.closeWindows}>
+                                    <p>Dor Bar</p>
+                                </InfoWindow>)}
+                            </Marker>
+                        )}
+                    }
+                )}
+            </GoogleMap>)
     }
-    
-    )
+);
 
 class MyFancyComponent extends React.PureComponent {
     state = {
         isMarkerShown: false,
-    }
+    };
 
     componentDidMount() {
         this.delayedShowMarker()
@@ -124,16 +87,17 @@ class MyFancyComponent extends React.PureComponent {
     }
 
     render() {
-        return ( <
-            MapComp isMarkerShown = {
-                this.state.isMarkerShown
-            }
-            onMarkerClick = {
-                this.handleMarkerClick
-            }
+        return (<MapComp isMarkerShown={this.state.isMarkerShown}
+                         onMarkerClick={this.handleMarkerClick}
+                         markers={this.props.markers}
+                         isOpen={this.props.isOpen}
+                         onToggleOpen={this.props.onToggleOpen}
+                         closeWindows={this.props.closeWindows}
             />
         )
     }
 }
 
-export default MapComp
+
+// export default MapComp
+export default MyFancyComponent
